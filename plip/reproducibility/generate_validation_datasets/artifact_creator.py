15eae018efbff2698be02253654245812a4464f8
Roba Al Majzoub <robamajzoub@gmail.com>
2025-04-24 12:22:01 +0400

    Update artifact_creator.py

--- plip/reproducibility/generate_validation_datasets/artifact_creator.py
+++ plip/reproducibility/generate_validation_datasets/artifact_creator.py
@@ -29,58 +29,57 @@ for i in range(len(df)):
 
     im_name = image_path.split("/")[-1].split(".")[0]
     ####### creating artfacts
-    # luminance(image_path, "contrast", "bright", im_name,dest_dir2)
-    # luminance(image_path, "contrast", "dark", im_name,dest_dir2)
-    # luminance(image_path, "brightness", "bright", im_name,dest_dir3)
-    # luminance(image_path, "brightness", "dark", im_name, dest_dir3)
+    luminance(image_path, "contrast", "bright", im_name,dest_dir2)
+    luminance(image_path, "contrast", "dark", im_name,dest_dir2)
+    luminance(image_path, "brightness", "bright", im_name,dest_dir3)
+    luminance(image_path, "brightness", "dark", im_name, dest_dir3)
     # counter += 2
     # # ##
-    # spottify(image_path, im_name, "small", dest_dir1)
-    # spottify(image_path, im_name, "large", dest_dir4)
+    spottify(image_path, im_name, "small", dest_dir1)
+    spottify(image_path, im_name, "large", dest_dir4)
     # counter += 4
 
-    # blur(image_path, im_name, dest_dir3)
+    blur(image_path, im_name, dest_dir3)
     # counter += 1
 
-    # elastic(image_path, im_name, dest_dir1)
+    elastic(image_path, im_name, dest_dir1)
     # counter += 4
 
-    # fatify(image_path, im_name, 0.5,dest_dir1)
-    # fatify(image_path, im_name, 1, dest_dir2)
-    # fatify(image_path, im_name, 1.5, dest_dir3)
-    # fatify(image_path, im_name, 2, dest_dir4)
+    fatify(image_path, im_name, 0.5,dest_dir1)
+    fatify(image_path, im_name, 1, dest_dir2)
+    fatify(image_path, im_name, 1.5, dest_dir3)
+    fatify(image_path, im_name, 2, dest_dir4)
     # counter += 1
 
 
-    # flipper(image_path, im_name, 1, dest_dir1)
-    # flipper(image_path, im_name, 2, dest_dir2)
-    # flipper(image_path, im_name, 3, dest_dir3)
-    # flipper(image_path, im_name, 4, dest_dir4)
+    flipper(image_path, im_name, 1, dest_dir1)
+    flipper(image_path, im_name, 2, dest_dir2)
+    flipper(image_path, im_name, 3, dest_dir3)
+    flipper(image_path, im_name, 4, dest_dir4)
     # counter += 4
 
 
-    # compress1(image_path, im_name, dest_dir1)
-    # compress2(image_path, im_name, 5, dest_dir4)
-    # compress2(image_path, im_name, 10, dest_dir3)
-    # compress2(image_path, im_name, 50, dest_dir2)
-    # compress2(image_path, im_name, 70, dest_dir1)
+    compress1(image_path, im_name, dest_dir1)
+    compress2(image_path, im_name, 5, dest_dir4)
+    compress2(image_path, im_name, 10, dest_dir3)
+    compress2(image_path, im_name, 50, dest_dir2)
+    compress2(image_path, im_name, 70, dest_dir1)
     # counter += 1
 
-    # rotator(image_path, im_name, 1, dest_dir1)
-    # rotator(image_path, im_name, 2, dest_dir2)
-    # rotator(image_path, im_name, 3, dest_dir3)
-    # rotator(image_path, im_name, 4, dest_dir4)
+    rotator(image_path, im_name, 1, dest_dir1)
+    rotator(image_path, im_name, 2, dest_dir2)
+    rotator(image_path, im_name, 3, dest_dir3)
+    rotator(image_path, im_name, 4, dest_dir4)
     # counter += 1
 
-    # squamous(image_path, im_name, 1, dest_dir1)
-    # squamous(image_path, im_name, 2, dest_dir2)
-    # squamous(image_path, im_name, 3, dest_dir3)
-    # squamous(image_path, im_name, 4, dest_dir4)
+    squamous(image_path, im_name, 1, dest_dir1)
+    squamous(image_path, im_name, 2, dest_dir2)
+    squamous(image_path, im_name, 3, dest_dir3)
+    squamous(image_path, im_name, 4, dest_dir4)
     # counter += 8
 
 
     if im_name not in finished_ids_1:
         threader(image_path, im_name, "small", dest_dir1)
-        # threader(image_path, im_name, "large", dest_dir4)
-        counter += 2
-print(f"Created ........ {counter} ............ spots  lvl 4  artifacts")
+        threader(image_path, im_name, "large", dest_dir4)
+        # counter += 4
